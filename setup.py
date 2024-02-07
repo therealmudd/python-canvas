3b3ebf31ef825e9482ea7d79cb47a658ed50bdaf
therealmudd <m.antonio0294@gmail.com>
2024-02-07 15:07:30 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='your_package_name',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='Mario Antonio',
+    author_email='m.antonio0294@gmail.com',
+    description='A fun tool to create a canvas in the terminal.',
+    url='https://github.com/therealmudd/python-canvas.git',
+)
